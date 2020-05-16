d7c38ffbcc730bfdc858dd6edc1a0d3b36e5b1f0
Tom Lee <jhtomlee@gmail.com>
2020-05-16 18:59:58 -0400

    Add ngRoute dependency and define app's routes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-var app = angular.module("NearMeApp", ['leaflet-directive'])
+var app = angular.module(
+
+  // name of AngularJS app
+  "NearMeApp", 
+
+  // dependencies
+  ['ngRoute','leaflet-directive']
+)
+
+// define app's routes:
+app.config(function ($routeProvider) { 
+  $routeProvider 
+    .when('/', { 
+      controller: 'MainController', 
+      templateUrl: 'views/main.html' 
+    })
+    .otherwise({ 
+      redirectTo: '/' 
+    }); 
+});
