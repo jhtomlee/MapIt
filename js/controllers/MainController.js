987db99e56aeb663a4a94112aea10c709dcb1f0d
Tom Lee <jhtomlee@gmail.com>
2020-05-17 14:42:11 -0400

    Implement dynamic searching for locations

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,24 +1,45 @@
-app.controller('MainController', ['$scope', 'places', function($scope, places) {
-  $scope.mapCenter = {
-    lat: 39.951860, //40.741934,
-    lng: -75.195130,//-74.004897,
-    zoom: 17
-  }
-  places.success(function(data) {
-    console.log("데이터", data)
-    $scope.geodata = data;
-    $scope.mapMarkers = geodataToMarkers($scope.geodata);
-  });
-  // $scope.mapMarkers = [
-  //   {
-  //     lat: 40.741389,
-  //     lng: -74.003056,
-  //     message: "111 Eighth Avenue"
-  //   },
-  //   {
-  //     lat: 40.7425,
-  //     lng: -74.006111,
-  //     message: "Chelsea Market"
-  //   }
-  // ]
+app.controller('MainController', ['$scope', '$mdSidenav', '$location','places','searchPlaces', function($scope, $mdSidenav, $location, places, searchPlaces) {
+  $scope.toggleSideBar = function() { 
+    $mdSidenav('left').toggle();
+  }; 
+  // $scope.latitude = '39.955002' ;
+  // $scope.longitude = '-75.200493' ;
+  $scope.latitude = '40.741934' ;
+  $scope.longitude = '-74.004897' ;
+
+  $scope.startLocation = function() { 
+
+    $scope.mapCenter = {
+      lat: 39.951860, //40.741934,
+      lng: -75.195130,//-74.004897,
+      zoom: 17
+    }
+    places.success(function(data) {
+      console.log("데이터", data)
+      $scope.geodata = data;
+      $scope.mapMarkers = geodataToMarkers($scope.geodata);
+      $location.path('/main')
+    });
+  }; 
+
+  $scope.searchLocation = function() { 
+    console.log('button pressed!: ', $scope.latitude, $scope.longitude)
+    $scope.toggleSideBar();
+    console.log(Number($scope.latitude), Number($scope.longitude))
+    $scope.mapCenter = {
+      lat: Number($scope.latitude), //39.955002, 
+      lng:  Number($scope.longitude), //-75.200493,
+      zoom: 17
+    }
+    $scope.$apply();
+    $location.path('/main')
+    // searchPlaces.success(function(data) {
+    //   console.log("데이터 search by location: ", data)
+    //   $scope.geodata = data;
+    //   $scope.mapMarkers = geodataToMarkers($scope.geodata);
+      
+    //   // $scope.$apply();
+    //   $location.path('/main')
+    // });
+  }; 
 }]);
--- js/services/places.js
+++ js/services/places.js
@@ -7,3 +7,13 @@ app.factory('places', ['$http', function ($http) {
       return err;
     });
 }]);
+
+app.factory('searchPlaces', ['$http', function ($http) {
+  return $http.jsonp('https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=5000&gscoord=39.955002%7C-75.200493&gslimit=30&format=json&callback=JSON_CALLBACK')
+    .success(function (data) {
+      return data;
+    })
+    .error(function (err) {
+      return err;
+    });
+}]);
